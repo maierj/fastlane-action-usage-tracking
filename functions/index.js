8086216820a3131a98bed3a5e17b268dea79cace
Jonas Maier <maierjonas@live.de>
2019-12-07 22:50:57 +0100

    implement notification email

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
+});
+
+exports.jfDjzewAZBKCNmdxUqkcvrCcRphfiVgshhcGQBzhtayQWraQhyhzbjFmxtbhCEWZ = functions.https.onRequest((req, res) => {
+    const mailOptions = {
+        from: "github.fastlane.action@gmail.com",
+        to: "maierjonas@live.de",
+    };
+
+    mailOptions.subject = "New fastlane action run";
+    mailOptions.text = `<ul><li>Runner OS: ${req.body.runnerOS}</li><li>Repository: ${req.body.repository}</li><li>UsesOptions: ${req.body.usesOptions}</li><li>UsesSubdirectory: ${req.body.usesSubdirectory}</li><li>UsesBundleInstallPath: ${req.body.usesBundleInstallPath}</li></ul>`;
+
+    try {
+        await mailTransport.sendMail(mailOptions);
+    } catch(error) {
+        console.error('There was an error while sending the email:', error);
+    }
+
+    return null;
+});
