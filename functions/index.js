99fd0737122f2a045a7ff4eb42dbb2c2dd585433
Jonas Maier <maierjonas@live.de>
2019-12-27 12:07:59 +0100

    Refactored function to store data in firestore

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,25 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
-
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
-const mailTransport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: gmailEmail,
-        pass: gmailPassword,
-    },
-});
+const {Firestore, Timestamp} = require('@google-cloud/firestore');
 
 exports.registerActionRun = functions.https.onRequest((req, res) => {
-    const mailOptions = {
-        from: "github.fastlane.action@gmail.com",
-        to: "maierjonas@live.de",
-    };
+	const firestore = new Firestore({
+		projectId: "github-fastlane-action"
+	});
 
-    mailOptions.subject = "New fastlane action run";
-    mailOptions.html = `<html><body><ul><li>Runner OS: ${req.body.runnerOS}</li><li>Repository: ${req.body.repository}</li><li>UsesOptions: ${req.body.usesOptions}</li><li>UsesSubdirectory: ${req.body.usesSubdirectory}</li><li>UsesBundleInstallPath: ${req.body.usesBundleInstallPath}</li></ul></body></html>`;
-
-    try {
-        mailTransport.sendMail(mailOptions);
-    } catch(error) {
-        console.error('There was an error while sending the email:', error);
-    }
-
-    res.end();
+	firestore.collection('action-runs').add({
+		runnerOS: req.body.runnerOS,
+		repository: req.body.repository,
+		usesOptions: req.body.usesOptions === "true",
+		usesSubdirectory: req.body.usesSubdirectory === "true",
+		usesBundleInstallPath: req.body.usesBundleInstallPath === "true",
+		created: Timestamp.fromDate(new Date())
+	}).then(ref => {
+		console.log('Added document with ID: ', ref.id);
+		res.end();
+	}).catch(err => {
+		console.log("Adding document failed: " + err);
+		res.end();
+	});
 });
